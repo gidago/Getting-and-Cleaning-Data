d3cf56e54fe1bed0ef4c38aa2039288b826a3522
gidago <dagoberto.acme@gmail.com>
2015-05-19 15:39:58 +0200

    Create run_analysis.R

---
+++ run_analysis.R
@@ -0,0 +1,23 @@
+# run_analysis.R
+# 19-May-2015
+# Coursera: Getting and Cleaning Data
+#
+#
+#
+# Data paths
+
+# Merge the training and the test sets to create one data set.
+
+# Assign column names to the merged data
+
+# Extract only the measurements on the mean and standard deviation for each measurement.
+
+# Use descriptive activity names to name the activities in the data set.
+
+# Label the data set with descriptive variable names.
+
+# Create an independent tidy data set with the average of each variable 
+
+# for each activity and each subject.
+
+# Write the tidy data set created to an output file
